346b76fd9353c8c19ee2f8cbbf925503134ec1d6
TranVo276 <tvo941352@gmail.com>
2024-09-04 10:20:41 -0700

    change 2

--- problem2.java
+++ problem2.java
@@ -3,9 +3,12 @@ public class problem2
 {
     public static void main(String[] ars)
     {
-        String name1 = "Sandy David Elaine Jesse";
+        String name1 = "Sandy ";
+        String name2 = "David ";
+        String name2 = "Elaine ";
+        String name4 = "Jesse ";
 
     
-        System.out.print(name1);
+        System.out.print(name4 + name2 + name3 + name1 );
     }
 }
